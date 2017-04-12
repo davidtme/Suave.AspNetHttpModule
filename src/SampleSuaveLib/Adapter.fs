210f38836c0d8ed1bf91ccb497df75dfae20cff7
David Teasdale <david@teasdale.me>
2017-04-12 14:04:17 +0100

    Clean up

--- src/SampleSuaveLib/Adapter.fs
+++ src/SampleSuaveLib/Adapter.fs
@@ -2,12 +2,12 @@
 
 open System.Web
 open Suave.AspNetHttpModule
-open SuaveTest
+open App
 
-type Handler() = 
+type Handler() =
     interface IHttpModule with
         member this.Init(application: HttpApplication) =
-            application.BeginRequest.Add(fun _ -> 
+            application.BeginRequest.Add(fun _ ->
                 if tryRunWebPartFromContext app application.Context then
                     application.Context.Response.End()
 
--- src/SampleSuaveLib/App.fs
+++ src/SampleSuaveLib/App.fs
@@ -4,8 +4,8 @@ open Suave
 open Suave.Filters
 open Suave.Operators
 
-let app = 
-    choose [ path "/api/test" >=> Successful.OK "Hello world!" ]
+let app =
+    choose [ path "/api/test" >=> Successful.OK "Hello from Suave!" ]
 
-    
+
 
