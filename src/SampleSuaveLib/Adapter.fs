a16130692a02893cf0b7b3bf225974f427a10109
David Teasdale <david@teasdale.me>
2017-04-12 16:42:42 +0100

    Add simple program

--- src/SampleSuaveLib/Adapter.fs
+++ src/SampleSuaveLib/Adapter.fs
@@ -2,15 +2,8 @@
 
 open System.Web
 open Suave.AspNetHttpModule
-open App
 
 type Handler() =
     interface IHttpModule with
-        member __.Init(application: HttpApplication) =
-            application.BeginRequest.Add(fun _ ->
-                if tryRunWebPartFromContext app application.Context then
-                    application.Context.Response.End()
-
-            )
-
+        member __.Init application = handleApplication App.app (* <-- your root web part *) application
         member __.Dispose() = ignore()
---
+++ src/SampleSuaveLib/Program.fs
@@ -0,0 +1,9 @@
+﻿module Program
+
+open Suave
+open SampleSuaveLib.App
+
+[<EntryPoint>]
+let main _ =
+    startWebServer defaultConfig app
+    0
